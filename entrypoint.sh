60e039c5e1571b7e4f9176a13440d26e12a416b9
Teagan glenn <teagan.m.glenn@gmail.com>
2019-12-23 19:25:31 -0700

    Try to install deps

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,14 +4,18 @@ set -Eeuo pipefail
 
 function print_help {
     echo "Available options:"
-    echo " start  - Start Rasa Action Server"
-    echo " help   - Print this help"
-    echo " run    - Run an arbitrary command inside the container"
+    echo " start        - Start Rasa Action Server"
+    echo " install_deps - Install dependencies for detected actions"
+    echo " help         - Print this help"
+    echo " run          - Run an arbitrary command inside the container"
 }
 
 case ${1} in
     start)
         exec python -m rasa_sdk "${@:2}"
+        ;;
+    install_deps)
+        find /app/actions -name requirements.txt -exec pip install -r  {} \
         ;;
     run)
         exec "${@:2}"
